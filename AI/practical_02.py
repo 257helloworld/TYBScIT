a991202af8b3779bb55f5cd0ca6024ffe1448aa0
257helloWorld <adityakadam567@gmail.com>
2023-07-05 21:08:44 +0530

    Commit

---
+++ AI/practical_02.py
@@ -0,0 +1,19 @@
+graph = {
+    'A': set(['B', 'C']),
+    'B': set(['A', 'D', 'E']),
+    'C': set(['A', 'F']),
+    'D': set(['B']),
+    'E': set(['B', 'F']),
+    'F': set(['C', 'E'])
+}
+
+visited = set()
+
+def dfs(visited,graph,root):
+    if root not in visited:
+        print(root)
+        visited.add(root)
+        for neighbour in graph[root]:
+            dfs(visited,graph,neighbour)
+
+dfs(visited ,graph,'A')
