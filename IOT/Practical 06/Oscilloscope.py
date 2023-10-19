6495eb8b498db5b4bbd4e3fdbd00a93a5d7f48f5
257helloWorld <adityakadam567@gmail.com>
2023-10-19 21:30:09 +0530

    Commit

--- IOT/Practical 06/Oscilloscope.py
+++ IOT/Practical 06/Oscilloscope.py
@@ -3,25 +3,24 @@ import matplotlib.pyplot as plt
 
 #import numpy
 from drawnow import *
-import Adafruit_ADS1x15
+import Adafruit_ADS1x15 as Adafruit
 
-adc = Adafruit_ADS1x15.ADS1115()
-
+adc = Adafruit.ADS1115()
 
 GAIN = 1
 val = []
-cnt = 0
 plt.ion()
 # Start continuous ADC conversions on channel 0 using the previous gain value.
 adc.start_adc(0, gain=GAIN)
-print('Reading ADS1x15 channel 0')
-#create the figure function
+print('Reading Channel 0')
 
+# Figure Function
 def makeFig():
     plt.ylim(-5000,5000)
     plt.title('Osciloscope')
     plt.grid(True)
     plt.ylabel('ADC outputs')
+    # ro- represents red circle and line.
     plt.plot(val, 'ro-', label='Channel 0')
     plt.legend(loc='lower right')
 
@@ -31,10 +30,8 @@ while(True):
     print('Channel 0: {0}'.format(value))
     # Sleep for half a second.
     time.sleep(0.5)
+    # Add new value to 'val' array
     val.append(int(value))
+    # Draw figure
     drawnow(makeFig)
-    plt.pause(.000001)
-    cnt = cnt+1
-    if(cnt>50):
-        val.pop(0)
 
